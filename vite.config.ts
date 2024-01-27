b0351da7a52a6a09d31a19e6f372959e8f53a5dc
wanglele <wanglele@luojilab.com>
2024-01-27 20:16:05 +0800

    feat: 学习环境变量的配置方式及原理

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+console.log('process', process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default ({ mode }) => {
+  console.log(loadEnv(mode, process.cwd()));
+  console.log('mode:', mode);
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  css: {
-    preprocessorOptions: {
-      less: {}
+    css: {
+      preprocessorOptions: {
+        less: {}
+      }
     }
-  }
-});
+  });
+};
