610a51288e460cfef1c1b92eed93981d13278299
wanglele <wanglele@luojilab.com>
2025-03-30 16:09:14 +0800

    chore: chore

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,14 +1,46 @@
 <template>
-  <wll-btn></wll-btn>
+  <button @click="handleOpenModal(1)">open modal with mask</button>
+  <hr />
+  <button @click="handleOpenModal(2)">open modal with nomask</button>
+  <hr />
+  <button @click="handleConsole">打印</button>
+  <hr />
+  <HalfScreenModal
+    :isModalVisible="isModalVisible"
+    :isMaskVisible="isMaskVisible"
+    @closeVisible="closeVisible"
+  />
 </template>
 
 <script setup lang="ts">
-import { defineCustomElement } from 'vue';
-import CustomVueCe from '@/components/CustomVue.ce.vue';
+import { ref, defineEmits } from 'vue';
+import HalfScreenModal from '../../components/HalfScreenModal.vue';
+const isModalVisible = ref(false);
+const isMaskVisible = ref(false);
 
-const Btn = defineCustomElement(CustomVueCe);
+const handleOpenModal = (flag) => {
+  switch (flag) {
+    case 1:
+      isModalVisible.value = true;
+      isMaskVisible.value = true;
+      break;
+    case 2:
+      isModalVisible.value = true;
+      isMaskVisible.value = false;
+      break;
+    default:
+      break;
+  }
+};
 
-window.customElements.define('wll-btn', Btn);
+const closeVisible = () => {
+  isModalVisible.value = false;
+  isMaskVisible.value = false;
+};
+
+const handleConsole = () => {
+  console.log('console.log');
+};
 </script>
 
-<style scoped lang="less"></style>
+<style scoped></style>
